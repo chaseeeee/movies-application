9add082f9cf625ce0f2f6f3a5134ad530f434c4c
chasehammock <chasehammock@gmail.com>
2017-09-13 15:49:03 -0500

    added the working stuff to the repo for the object page to display

--- src/index.js
+++ src/index.js
@@ -8,12 +8,25 @@ sayHello('World');
  * require style imports
  */
 const getMovies = require('./getMovies.js');
+const movieDisplay = document.getElementById("movieDisplay");
+
+/* GET THESE MOVIES */
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+    movieDisplay.innerHTML =`id#${id} - ${title} - rating: ${rating}`;
+
+    /* DISPLAY THE MOVIES ON THE PAGE  */
   });
+
+
+
+
+  /* CATCH THE ERRORS AND DISPLAY OOPS*/
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
