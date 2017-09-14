cf75342a1c3087bf6f2c717f4152894211e7ee22
chasehammock <chasehammock@gmail.com>
2017-09-14 11:52:21 -0500

    added the latest changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ sayHello('World');
  */
 const getMovies = require('./getMovies.js');
 const movieDisplay = document.getElementById("movieDisplay");
+const movieSubmit = document.getElementById("movieSubmit");
+const movieObject = {};
 
 /* GET THESE MOVIES */
 
@@ -19,17 +21,26 @@ getMovies().then((moviesData) => {
     moviesData.forEach(({title, rating, id}) => {
         console.log(`id#${id} - ${title} - rating: ${rating}`);
 
-        movies += `id#${id} - ${title} - rating: ${rating} <br>`;
-
+        movies += `<tr><td>id#${id} </td> ${title} <td> rating: ${rating}</td></tr>`;
 
 
         /* DISPLAY THE MOVIES ON THE PAGE  */
     });
 
-    movieDisplay.innerHTML = `${movies}`;
+    movieDisplay.innerHTML = `<table class="table">${movies}</table>`;
     /* CATCH THE ERRORS AND DISPLAY OOPS*/
 
 }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    /*alert('Oh no! Something went wrong.\nCheck the console for details.')*/
     console.log(error);
 });
+
+ movieSubmit.click(addMovie);
+
+ function addMovie() {
+     let movieForm = movieSubmit.values();
+     /*ratingvalue.concat(movieObject);*/
+     console.log("ADDED MOVIES");
+ };
+
+
